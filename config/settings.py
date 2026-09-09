b8b326f37f01039d5b6bbf8b53be5cf3b52f125c
techademyAmit <Amit.Raghunath@techademy.com>
2026-09-09 12:51:56 +0530

    adding settings.py

--- config/settings.py
+++ config/settings.py
@@ -78,13 +78,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.getenv("POSTGRES_DB"),
+#         "USER": os.getenv("POSTGRES_USER"),
+#         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB"),
-        "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": "postgres",
+        "NAME": "docker_demo",
+        "USER": "postgres",
+        "PASSWORD": "mypass",
+        "HOST": "localhost",
         "PORT": "5432",
     }
 }
