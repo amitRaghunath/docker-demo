92fa9223cdf4a8f5472b53b65e8cb8f522dc274c
techademyAmit <Amit.Raghunath@techademy.com>
2026-09-10 15:28:04 +0530

    Added api checks

--- users/tests.py
+++ users/tests.py
@@ -1,7 +1,26 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from users.models import User
+class UserAPITest(APITestCase):
 
+    def test_create_user(self):
+        data = {
+            "name": "Amit",
+            "email": "amit@test.com",
+            "age": 25
+        }
 
-class UserAPITest(TestCase):
+        response = self.client.post(
+            "/v1/users",
+            data,
+            format="json"
+        )
 
-    def test_something(self):
-        self.assertEqual(1 + 1, 2)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_201_CREATED
+        )
+
+        self.assertTrue(
+            User.objects.filter(email="amit@test.com").exists()
+        )
